108452cabe71037322c9a93fbf165d83ea572df5
Zadielerick <zadielerick@gmail.com>
2016-09-28 14:58:23 -0500

    Pipes and fork setup

--- Project1.c
+++ Project1.c
@@ -1,23 +1,42 @@
 #include <stdio.h>
 #include <sys/types.h>
+#include <unistd.h>
 
-void CPU(void);
-void Memory(void);
+void CPU(int *mem_pipe, int *cpu_pipe);
+void Memory(int *mem_pipe, int *cpu_pipe);
 
 int main() {
+    /**
+     * memory_pipe: (from memory to CPU)
+     *          [0] : input for CPU
+     *          [1] : output for memory
+     * cpu_pipe:    (from CPU to memory)
+     *          [0] : input for memory
+     *          [1] : output for CPU
+     */
+    int memory_pipe[2];
+    int cpu_pipe[2];
+    
+    pipe(cpu_pipe);
+    pipe(memory_pipe);
+
     pid_t pid;
     pid = fork();
 
     if(pid == 0)
-        CPU();
+        CPU(memory_pipe, cpu_pipe);
     else
-        Memory();
+        Memory(memory_pipe, cpu_pipe);
 }
 
-void CPU(){
-    
+void CPU(int *mem_pipe, int *cpu_pipe){
+    close(mem_pipe[1]);         //Don't need output for memory
+    close(cpu_pipe[0]);         //Don't need input for memory
+    int PC,SP,IR,AC,X,Y;
+       
 }
 
-void Memory(){
-
+void Memory(int *mem_pipe, int *cpu_pipe){
+    close(mem_pipe[0]);         //Don't need input for CPU    
+    close(cpu_pipe[1]);         //Don't need output for CPU
 }
